354469ab63d13f898fc4c19f2e9887153706de52
Crooks2k <ciberronaldo@hotmail.com>
2022-12-13 18:31:29 -0500

    Deploy prepare

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://Crooks2k.github.io/react-api-consume",
   plugins: [react()],
 })
